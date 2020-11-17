cc52d2a61a34b847977bac8b1da25d1946cf5c1a
edlin andrea cardona molina <andrea_1578@hotmail.com>
2020-11-17 09:09:25 -0600

    Creacion de prop de la clase tarjeta de credito

--- Models/TarjetaDeCredito.cs
+++ Models/TarjetaDeCredito.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +10,22 @@ namespace BackEndv2.Models
 {
     public class TarjetaDeCredito
     {
+       [Key] 
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Titular { get; set; }
 
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string NumeroTarjeta { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string FechaExpiracion { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
     }
 }
