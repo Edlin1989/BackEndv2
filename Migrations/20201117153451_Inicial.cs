8e79bd591e61767c9953f1414b6e9f3ff15b1d30
edlin andrea cardona molina <andrea_1578@hotmail.com>
2020-11-17 09:41:48 -0600

    Agregar

---
+++ Migrations/20201117153451_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEndv2.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TarjetaDeCreditos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titular = table.Column<string>(type: "varchar(100)", nullable: false),
+                    NumeroTarjeta = table.Column<string>(type: "varchar(16)", nullable: false),
+                    FechaExpiracion = table.Column<string>(type: "varchar(5)", nullable: false),
+                    CVV = table.Column<string>(type: "varchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TarjetaDeCreditos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TarjetaDeCreditos");
+        }
+    }
+}
